22a61cb3fd5eded798a30e2843bf26e7cad9df56
caseymm <caseymm@gmail.com>
2025-05-12 17:38:17 -0400

    ugh

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.scss";
 import data from "./data.json";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h3>Latest</h3>
