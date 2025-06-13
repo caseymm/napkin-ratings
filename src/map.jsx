6143218f8345f07bc7fba74111a22e3e43267a01
caseymm <caseymm@gmail.com>
2025-06-13 19:27:07 -0400

    map temp

--- src/map.jsx
+++ src/map.jsx
@@ -17,7 +17,7 @@ export default function Map() {
 
     const map = new maplibregl.Map({
       container: mapRef.current,
-      center: [-73.93, 40.73], // Example coordinates (New York City)
+      center: [-78.7224957, 35.7836484],
       zoom: 12,
       style: {
         version: 8,
@@ -34,6 +34,46 @@ export default function Map() {
         },
         layers: layers("protomaps", namedFlavor("light"), { lang: "en" }),
       },
+    });
+
+    map.on("load", () => {
+      map.addSource("marker-source", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              geometry: {
+                type: "Point",
+                coordinates: [-78.7224957, 35.7836484],
+              },
+              properties: {
+                title: "My Marker",
+              },
+            },
+          ],
+        },
+      });
+
+      map.addLayer({
+        id: "marker-layer",
+        type: "circle",
+        source: "marker-source",
+        // layout: {
+        //   "icon-image": "custom-marker",
+        //   "icon-size": 0.5,
+        //   "text-field": "{title}",
+        //   "text-offset": [0, 1.25],
+        //   "text-anchor": "top",
+        // },
+        paint: {
+          "circle-radius": 8,
+          "circle-color": "#FF5722",
+          "circle-stroke-width": 2,
+          "circle-stroke-color": "#ffffff",
+        },
+      });
     });
 
     return () => map.remove();
