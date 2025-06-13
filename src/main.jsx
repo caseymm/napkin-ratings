3f8e4257298de40532359090f5f5dd8b7fcdd5d9
caseymm <caseymm@gmail.com>
2025-06-13 18:51:10 -0400

    map link

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./index.scss";
+
 import App from "./App.jsx";
 import Map from "./map.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1>Napkin Ratings</h1>
-    <App />
-    {/* <Map /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="app-list">
+              <h1>Napkin Ratings</h1>
+              <App />
+            </div>
+          }
+        />
+        <Route path="/map" element={<Map />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
